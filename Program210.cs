ec4c272941b55a501ad3b7f3529f291e49120ccd
Taaniyah <122187661+Taaniyah@users.noreply.github.com>
2023-01-21 17:05:45 +0200

    Add files via upload

---
+++ Program210.cs
@@ -0,0 +1,91 @@
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+     Console.WriteLine("class, Journal");
+        
+    
+}
+object value = Console.WriteLine("class, Journal");
+             
+var name = Console.ReadLine();
+var currentDate = DateTime.Now;
+}
+Console.WriteLine($"{Environment.NewLine}Hello, {name}, on {currentDate:d} at {currentDate:t}!");
+Console.Write($"{Environment.NewLine}Press any key to exit...");
+Console.ReadKey(true);
+  
+  static void Main(string[] args)
+    {
+        DisplayWelcomeMessage();
+
+        string FirstName = PromptUserName();
+        int Surname = PromptUserNumber();
+
+        int PropertyGroup = SquareNumber(number: 9);
+
+        DisplayResult(name: FirstName, square: Surname);
+    }
+
+    static void DisplayWelcomeMessage()
+    {
+        Console.WriteLine(value: "Welcome to your journal!");
+    }
+
+    static string PromptUserName()
+    {
+        Console.Write(value: "Please enter your name: ");
+        string name = Console.ReadLine();
+
+        return name;
+    }
+
+    static int PromptUserNumber()
+    {
+        Console.Write(value: "Who was the most interesting person I interacted with today: ");
+        int number = int.Parse(s: Console.ReadLine());
+
+        return number;
+    }
+
+    static int SquareNumber(int number)
+    {
+        int square = number * number;
+        return square;
+    }
+
+    static void DisplayResult(string name, int square)
+    {
+        Console.WriteLine(value: $"{name}, the square of your number is {square}"
+     
+     static void Main(string[] args)
+    {
+        Console.WriteLine(value:'class Journal)
+{
+
+string entry = 'Name','Surname','Date','Members','classes'
+ outputFile.WriteLine("This will be the first colum in the file.");
+
+ var logger = require('password')
+ var apps = journal();
+
+ string message = "";
+  if (args.length < 1) {
+    message = "welcom to the. C#!";
+    }
+    else {
+        for each (string item in args) {
+            message += item
+    foreach (string line in lines)
+{
+    string[] parts = line.Split(separator: ",");
+
+    string firstName = parts[0];
+    string lastName = parts[1];
+}
+
+
+
+
